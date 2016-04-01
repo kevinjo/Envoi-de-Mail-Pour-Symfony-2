30991572caa62fae7ebd69fef395fa7200fb802b
Kraddle <kraddle@me.com>
2016-04-01 13:39:52 +0200

    Update MonController.php

--- MonController.php
+++ MonController.php
@@ -4,13 +4,13 @@ $serviceMail = $this->get('app.mail');
 
 $mail= $serviceMail->send('Sujet',
 		array('destinataire','autre destinataire'),
-		'expediteur',
-		'AppMonBundle:Default:index.mail.twig',
+			'expediteur',
+			'AppMonBundle:Default:index.mail.twig',
 		array(
-		'name'=>'John Doe',
-		'message' =>'Parturient Egestas Nibh Sem Parturient Egestas Nibh Sem',
+			'name'=>'John Doe',
+			'message' =>'Parturient Egestas Nibh Sem Parturient Egestas Nibh Sem',
+		),
 		array(), //attachment
-		)
 	);
 
 	if($mail){
